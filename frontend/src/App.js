bb77a9227bd8c7502873db357ca8f61b3628c16a
dkq <82295670+dkqdkq@users.noreply.github.com>
2024-05-07 08:43:17 +0900

    add dinner

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import { useState, useEffect } from 'react';
-
+import logo from "./logo.png";
+import "./App.css";
+import React from "react";
+import { useState, useEffect } from "react";
 
 function App() {
     return (
@@ -14,24 +13,27 @@ function App() {
     );
 }
 
-
 function Body() {
-    let [today, setToday] = useState('');
-    let [meal, setMeal] = useState('');
-    let [cal, setCal] = useState('');
+    let [today, setToday] = useState("");
+    let [meal, setMeal] = useState("");
+    let [cal, setCal] = useState("");
+    let [mealType, setMealType] = useState(0);
 
-
-    useEffect(() => {
-        const { today } = getDates();
-        setToday(today);
-        fetch(`/lunch?today=${today}`)
+    function getMeal(today) {
+        fetch(`/meal?today=${today}`)
             .then((res) => res.json())
             .then(({ meal, cal }) => {
                 setMeal(meal);
                 setCal(cal);
             });
+    }
+
+    useEffect(() => {
+        const { today } = getDates();
+        setToday(today);
+        getMeal(today);
     }, []);
-    
+
     console.log(meal && today === getDates().today);
 
     return (
@@ -39,111 +41,128 @@ function Body() {
             <div className="meal">
                 <div className="meal-title">
                     <div className="meal-title-date">
-                        <button onClick={() => {
-                            const { yesterday } = getDates(today);
-                            setToday(yesterday);
-                            fetch(`/lunch?today=${yesterday}`)
-                                .then((res) => res.json())
-                                .then(({ meal, cal }) => {
-                                    setMeal(meal);
-                                    setCal(cal);
-                                });
-                        }}><i className="bi bi-caret-left-fill"></i></button>
-                        <p>{today.slice(0, 4)}년 {today.slice(4, 6)}월 {today.slice(6)}일 ({ getDayOfWeek(today) })</p>
-                        <button onClick={() => {
-                            const { tomorrow } = getDates(today);
-                            setToday(tomorrow);
-                            fetch(`/lunch?today=${tomorrow}`)
-                                .then((res) => res.json())
-                                .then(({ meal, cal }) => {
-                                    setMeal(meal);
-                                    setCal(cal);
-                                });
-                        }}><i className="bi bi-caret-right-fill"></i></button>
+                        <button
+                            onClick={() => {
+                                const { yesterday } = getDates(today);
+                                setToday(yesterday);
+                                getMeal(yesterday);
+                            }}
+                        >
+                            <i className="bi bi-caret-left-fill"></i>
+                        </button>
+                        <p>
+                            {today.slice(0, 4)}년 {today.slice(4, 6)}월{" "}
+                            {today.slice(6)}일 ({getDayOfWeek(today)})
+                        </p>
+                        <button
+                            onClick={() => {
+                                const { tomorrow } = getDates(today);
+                                setToday(tomorrow);
+                                getMeal(tomorrow);
+                            }}
+                        >
+                            <i className="bi bi-caret-right-fill"></i>
+                        </button>
                     </div>
+                </div>
+                <div className="meal-type-selector">
+                    <button
+                        className="meal-type-button active"
+                        onClick={(e) => {
+                            setMealType(0);
+                            e.target.classList.add("active");
+                            e.target.nextSibling.classList.remove("active");
+                        }}
+                    >
+                        중식
+                    </button>
+                    <button
+                        className="meal-type-button"
+                        onClick={(e) => {
+                            setMealType(1);
+                            e.target.classList.add("active");
+                            e.target.previousSibling.classList.remove("active");
+                        }}
+                    >
+                        석식
+                    </button>
                 </div>
                 <div className="meal-content">
                     <div className="meal-content-meal">
                         <h2>급식</h2>
-                        {
-                            meal ? meal.map((element, index) => {
+                        {meal ? (
+                            meal[mealType].map((element, index) => {
                                 return (
-                                    <div className="meal-content-meal-element" key={index}>
+                                    <div
+                                        className="meal-content-meal-element"
+                                        key={index}
+                                    >
                                         <p key={index}>{element}</p>
                                     </div>
-                                    );
-                            }) : <p>오늘은 급식이 없습니다</p>
-                        }
+                                );
+                            })
+                        ) : (
+                            <p>오늘은 급식이 없습니다</p>
+                        )}
                     </div>
                     <div className="meal-content-cal">
                         <h2>칼로리</h2>
-                        {
-                            cal ? <p>{cal}</p> : <p>오늘은 급식이 없습니다.</p>
-                        }
+                        {cal ? (
+                            <p>{cal[mealType]}</p>
+                        ) : (
+                            <p>오늘은 급식이 없습니다.</p>
+                        )}
                     </div>
                 </div>
             </div>
-            {
-                meal && today === getDates().today ?
-                    <div className="meal-feedback">
-                        <p>오늘의 급식을 평가해주세요!</p>
-                        <div className="meal-feedback-buttons">
-                            <button className="feedback-button feedback-bad">
-                                <i className="bi bi-emoji-frown"></i>
-                                <span className="feedback-count feedback-count-bad">5</span>
-                            </button>
-                            <button className="feedback-button feedback-normal">
-                                <i className="bi bi-emoji-neutral"></i>
-                                <span className="feedback-count feedback-count-normal">5</span>
-                            </button>
-                            <button className="feedback-button feedback-good">
-                                <i className="bi bi-emoji-smile"></i>
-                                <span className="feedback-count feedback-count-good">5</span>
-                            </button>
-                        </div>
-                    </div>
-                    : null
-            }
-            <div></div>
         </div>
     );
 }
-
 
 function Header() {
     return (
         <div className="App-header">
             <h1>양천고 급식</h1>
-        </div >
+        </div>
     );
 }
-
 
 function Footer() {
     return (
         <div className="App-footer">
-            <p className="copyright">
-                &copy; 2021 Yangcheon High School
-            </p>
+            <p className="copyright">&copy; 2024 Yangcheon High School</p>
         </div>
-    )
+    );
 }
 
-
-function getDates(dateString = '') {
-    const date = dateString ? new Date(dateString.slice(0, 4), dateString.slice(4, 6) - 1, dateString.slice(6)) : new Date();
+function getDates(dateString = "") {
+    const date = dateString
+        ? new Date(
+              dateString.slice(0, 4),
+              dateString.slice(4, 6) - 1,
+              dateString.slice(6)
+          )
+        : new Date();
     const yesterday = new Date(date.setDate(date.getDate() - 1));
     const today = new Date(date.setDate(date.getDate() + 1));
     const tomorrow = new Date(date.setDate(date.getDate() + 1));
     return {
-        yesterday: `${yesterday.getFullYear()}${('0' + (yesterday.getMonth() + 1)).slice(-2)}${('0' + yesterday.getDate()).slice(-2)}`,
-        today: `${today.getFullYear()}${('0' + (today.getMonth() + 1)).slice(-2)}${('0' + today.getDate()).slice(-2)}`,
-        tomorrow: `${tomorrow.getFullYear()}${('0' + (tomorrow.getMonth() + 1)).slice(-2)}${('0' + tomorrow.getDate()).slice(-2)}`,
+        yesterday: `${yesterday.getFullYear()}${(
+            "0" +
+            (yesterday.getMonth() + 1)
+        ).slice(-2)}${("0" + yesterday.getDate()).slice(-2)}`,
+        today: `${today.getFullYear()}${("0" + (today.getMonth() + 1)).slice(
+            -2
+        )}${("0" + today.getDate()).slice(-2)}`,
+        tomorrow: `${tomorrow.getFullYear()}${(
+            "0" +
+            (tomorrow.getMonth() + 1)
+        ).slice(-2)}${("0" + tomorrow.getDate()).slice(-2)}`,
     };
 }
 
 function getDayOfWeek(dateString) {
-    const daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+    const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
     const year = dateString.slice(0, 4);
     const month = Number(dateString.slice(4, 6)) - 1;
     const day = dateString.slice(6, 8);
--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-app.get("/lunch", async (req, res) => {
+app.get("/meal", async (req, res) => {
     const today = req.query.today;
     console.log(today);
     const url =
@@ -41,22 +41,41 @@ app.get("/lunch", async (req, res) => {
                     message: "데이터가 없습니다.",
                 });
                 return;
+            } else if (response.data.RESULT.CODE !== "INFO-000") {
+                res.send({
+                    message: "오류가 발생했습니다.",
+                });
+                console.log(response.data.RESULT.CODE);
+                return;
             }
         }
-        const { DDISH_NM, CAL_INFO } = mealServiceDietInfo[1].row[0];
+        const lunch_DDISH_NM = mealServiceDietInfo[1].row[0].DDISH_NM;
+        const dinner_DDISH_NM = mealServiceDietInfo[1].row[1].DDISH_NM;
+        const lunch_CAL_INFO = mealServiceDietInfo[1].row[0].CAL_INFO;
+        const dinner_CAL_INFO = mealServiceDietInfo[1].row[1].CAL_INFO;
+
         const regex =
             /\([^()]+\)|\/자율|\(완\)|\(선\)|\(교\)|\(주\)|\(해당없음\)|\(양천\)/g;
-        const mealDataString = DDISH_NM.replace(/\([^()]+\)|/, "").replace(
-            regex,
-            ""
-        );
-        const mealData = mealDataString
+
+        const lunchMealDataString = lunch_DDISH_NM
+            .replace(/\([^()]+\)|/, "")
+            .replace(regex, "");
+
+        const lunchMealData = lunchMealDataString
+            .split("<br/>")
+            .map((element) => element.trim());
+
+        const dinnerMealDataString = dinner_DDISH_NM
+            .replace(/\([^()]+\)|/, "")
+            .replace(regex, "");
+
+        const dinnerMealData = dinnerMealDataString
             .split("<br/>")
             .map((element) => element.trim());
 
         res.send({
-            meal: mealData,
-            cal: CAL_INFO,
+            meal: [lunchMealData, dinnerMealData],
+            cal: [lunch_CAL_INFO, dinner_CAL_INFO],
         });
     } catch (error) {
         console.error(error);
